5dd73f74815648e1a452146dc319c228694730ff
k4kotlin <k4kotlin@gmail.com>
2018-07-27 11:23:04 +0530

    arithmetic trick second with variable names

--- main.py
+++ main.py
@@ -8,3 +8,12 @@ print(3**4) # 3*3 = 9*9 = 81
 #  of side 25 cm, it the side of each brick is 5 cm
 
 print((25*25) / (5*5))
+
+
+#There are 500 students at the high school.
+#there are only two menu options: chicken or fish.
+#If 15% students ordered fish, how many students ordered chicken?
+
+total_student = 500
+number_of_students_ordered_chicken = 85/100 * total_student
+print("Ordered chicken ", number_of_students_ordered_chicken)
