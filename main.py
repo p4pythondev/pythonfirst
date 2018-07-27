2faf70e7ba4af3634ccbbf167d33e717bbcf6aad
k4kotlin <k4kotlin@gmail.com>
2018-07-27 11:08:32 +0530

    arithmetic trick

---
+++ main.py
@@ -0,0 +1,10 @@
+print(18/3) # get value with fload number 6.0
+print(18//3) # get value without fload 6
+
+print(3*3) # get 9
+print(3**4) # 3*3 = 9*9 = 81
+
+#find the number of bricks to be laid in a square path
+#  of side 25 cm, it the side of each brick is 5 cm
+
+print((25*25) / (5*5))
